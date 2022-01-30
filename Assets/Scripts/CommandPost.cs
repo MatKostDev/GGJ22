67d663d3dd322cb793ee8cc11ba1e327feb0fe5f
MatK <mathew.kostrzewa@ontariotechu.net>
2022-01-30 14:53:27 -0500

    Adding units to command posts

--- Assets/Scripts/CommandPost.cs
+++ Assets/Scripts/CommandPost.cs
@@ -7,6 +7,9 @@ public class CommandPost : MonoBehaviour
     [SerializeField]
     Faction faction;
 
+    [SerializeField]
+    int requiredUnitsForCapture = 5;
+
     int m_enemiesAdded;
     int m_alliesAdded;
 
@@ -22,14 +25,69 @@ public class CommandPost : MonoBehaviour
 
     void OnTriggerEnter(Collider a_other)
     {
-        if (!a_other.TryGetComponent<Faction>(out var faction))
+        if (!a_other.TryGetComponent<Faction>(out var otherFaction))
         {
             return;
         }
 
         if (faction.FactionType == FactionType.Neutral)
         {
-            
+            if (otherFaction.FactionType == FactionType.Enemy)
+            {
+                if (m_alliesAdded > 0)
+                {
+                    m_alliesAdded--;
+                    OnAddedUnit(a_other.gameObject);
+                }
+                else
+                {
+                    m_enemiesAdded++;
+                    OnAddedUnit(a_other.gameObject);
+                }
+            }
+            else if (otherFaction.FactionType == FactionType.Friendly)
+            {
+                if (m_enemiesAdded > 0)
+                {
+                    m_enemiesAdded--;
+                    OnAddedUnit(a_other.gameObject);
+                }
+                else
+                {
+                    m_alliesAdded++;
+                    OnAddedUnit(a_other.gameObject);
+                }
+            }
+        }
+        else if (faction.FactionType == FactionType.Friendly)
+        {
+            if (otherFaction.FactionType == FactionType.Enemy)
+            {
+                m_enemiesAdded++;
+                OnAddedUnit(a_other.gameObject);
+            }
+        }
+        else if (faction.FactionType == FactionType.Enemy)
+        {
+            if (otherFaction.FactionType == FactionType.Friendly)
+            {
+                m_alliesAdded++;
+                OnAddedUnit(a_other.gameObject);
+            }
+        }
+    }
+
+    void OnAddedUnit(GameObject a_unitObject)
+    {
+        Destroy(a_unitObject);
+
+        if (m_alliesAdded >= requiredUnitsForCapture)
+        {
+            ChangeFaction(FactionType.Enemy);
+        }
+        else if (m_enemiesAdded >= requiredUnitsForCapture)
+        {
+            ChangeFaction(FactionType.Friendly);
         }
     }
 }
