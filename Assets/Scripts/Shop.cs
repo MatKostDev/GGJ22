b7119c596c34ea39e3c725e32c837bb783957561
MatK <mathew.kostrzewa@ontariotechu.net>
2022-01-30 13:17:02 -0500

    Purchase cooldown w/ visual bar

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour
 {
@@ -17,7 +18,22 @@ public class Shop : MonoBehaviour
     [SerializeField]
     int unitsCost;
 
+    [SerializeField]
+    Image turretCooldownImage;
+
+    [SerializeField]
+    Image unitCooldownImage;
+
+    [SerializeField]
+    float turretCooldown;
+
+    [SerializeField]
+    float unitCooldown;
+
     ResourceManager m_resourceManager;
+
+    float m_turretTimer;
+    float m_unitTimer;
 
     void Awake()
     {
@@ -27,19 +43,41 @@ public class Shop : MonoBehaviour
         m_resourceManager = ResourceManager.Instance;
     }
 
+    void Update()
+    {
+        m_turretTimer = Mathf.Max(0f, m_turretTimer - Time.deltaTime);
+        m_unitTimer   = Mathf.Max(0f, m_unitTimer   - Time.deltaTime);
+
+        turretCooldownImage.fillAmount = m_turretTimer / turretCooldown;
+        unitCooldownImage.fillAmount   = m_unitTimer   / unitCooldown;
+    }
+
     public void TryBuyTurret()
     {
-        if (m_resourceManager.CurrentAmount < turretsCost)
+        if (m_resourceManager.CurrentAmount < turretsCost || m_turretTimer > 0.001f)
         {
             return;
         }
 
         m_resourceManager.SpendResources(turretsCost);
         TurretSpawner.Instance.SpawnTurret();
+
+        m_turretTimer = turretCooldown;
+
+        turretCooldownImage.fillAmount = 1f;
     }
 
     public void TryBuyUnits()
     {
+        if (m_resourceManager.CurrentAmount < turretsCost || m_unitTimer > 0.001f)
+        {
+            return;
+        }
 
+        m_resourceManager.SpendResources(unitsCost);
+
+        m_unitTimer = unitCooldown;
+
+        unitCooldownImage.fillAmount = 1f;
     }
 }
